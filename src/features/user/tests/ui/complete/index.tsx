a42895c0d8c816c871e846d62c0191200a90cbde
Alisher <zumanovaliser814@gmail.com>
2026-01-18 11:26:53 +0600

    yes

--- src/features/user/tests/ui/complete/index.tsx
+++ src/features/user/tests/ui/complete/index.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { useParams } from "next/navigation";
+import { useMemo } from "react";
+import { useParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import { useGetTestResult } from "@/entities/user/tests/model/api/queries";
 import { Button, cn, Spinner } from "@heroui/react";
 import { ErrorBlock } from "@/shared/ui/error-block";
 import { MoveRight } from "lucide-react";
+import { SeverityLevel } from "@/entities/user/tests/model/types";
 
 export const CompleteTest = () => {
   const { testId } = useParams<{ testId: string }>();
+  const router = useRouter();
 
   const {
     data: result,
@@ -15,6 +18,35 @@ export const CompleteTest = () => {
     isError,
     refetch,
   } = useGetTestResult(testId);
+
+  const ui = useMemo(() => {
+    const level = (result?.severityLevel ?? "low") as SeverityLevel;
+
+    const map: Record<SeverityLevel, { label: string; color: string }> = {
+      low: {
+        label: "Төмөн",
+        color: "#22C55E",
+      },
+      moderate: {
+        label: "Орточо",
+        color: "#F59E0B",
+      },
+      high: {
+        label: "Жогору",
+        color: "#FB923C",
+      },
+      critical: {
+        label: "Өтө жогору",
+        color: "#EF4444",
+      },
+    };
+
+    return map[level];
+  }, [result?.severityLevel]);
+
+  const goToCourses = () => {
+    router.replace("/user/courses");
+  };
 
   return (
     <div className="flex flex-col items-center py-10 px-5">
@@ -34,8 +66,9 @@ export const CompleteTest = () => {
       ) : (
         <div style={{ maxWidth: 472 }} className="flex flex-col items-center">
           <div className="h-5" />
+
           <p className="text-blue-700 font-bold text-3xl">
-            {result?.scorePercentage}%
+            {result?.scorePercentage}
           </p>
 
           <div className="h-5" />
@@ -45,18 +78,18 @@ export const CompleteTest = () => {
             style={{
               fontWeight: 700,
               fontSize: 48,
+              color: ui.color,
             }}
-            className="text-green-500"
           >
-            {result?.severityLevel}
+            {ui.label}
           </p>
 
           <p className="text-center text-neutral-500 mt-5">
-            {result?.recommendation}
+            {result?.recommendation}%
           </p>
 
           <Button
-            type="submit"
+            onPress={goToCourses}
             style={{ marginTop: 20 }}
             className={cn(
               "w-full h-fit rounded-xl bg-blue-700 text-white font-medium text-sm lg:text-xl py-3 lg:py-4.5"
